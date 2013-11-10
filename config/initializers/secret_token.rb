a51026e01a1e350abb5ec0b92cfef4a2d420d64d
drakeshs <drakeshs@drakeshs-Lenovo-G580.(none)>
2013-11-10 18:05:57 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sms::Application.config.secret_token = '446234f88a3ba965f1701c246a7616e2f33d76ce757b7c2dff344a20786df2d3a05fa0d60a560ed4b2ffdb14b1535d3bbbfe0ca02d2e4ff568857ee4f132126d'
